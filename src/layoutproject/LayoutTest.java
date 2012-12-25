d53d722b7664c0da655e883432e140946c873278
Blair Archibald <mrblairarchibald@googlemail.com>
2012-12-25 17:08:19 +0000

    Refactored Test Cases for readability

--- src/layoutproject/LayoutTest.java
+++ src/layoutproject/LayoutTest.java
@@ -18,43 +18,53 @@ public class LayoutTest {
 
 	@Test
 	public void noChildrenAndSomeContentHasContentsSize() {
-		MapNode node = new TestNode();
 		TestContentNode content = new TestContentNode();
-		node.setContent(content);
 		Size contentSize = new Size(1, 1);
 		content.setSize(contentSize);
+		
+		MapNode node = new TestNode();
+		node.setContent(content);
+		
 		Layout layout = new Layout(node);
 		layout.doLayout();
+		
 		assertThat(node.getSize(), is(contentSize.addSpaceAround(Layout.SPACE_AROUND)));
 		assertThat(node.getContent().getPosition(), is (new Position(Layout.SPACE_AROUND, Layout.SPACE_AROUND)));
 	}
 	
 	@Test
 	public void oneChildAndNoVisibleContentHasChildSize() {
-		TestNode node = new TestNode();
-		node.setContentVisible(false);
 		TestNode child = new TestNode();
 		Size childSize = new Size(1,1);
 		child.setSize(childSize);
+		
+		TestNode node = new TestNode();
 		node.addChild(child);
+		node.setContentVisible(false);
+		
 		Layout layout = new Layout(node);
 		layout.doLayout();
+		
 		assertThat(node.getSize(), is(childSize));
 		assertThat(node.getChild(0).getPosition(), is (new Position(0, 0)));
 	}
 	
 	@Test
 	public void oneChildAndHasVisibleContentHasChildSizePlusContentAndGap() {
-		TestNode node = new TestNode();
-		node.setContentVisible(true);
-		TestNode child = new TestNode();
 		Size childSize = new Size(1,1);
-		child.setSize(childSize);
-		TestContentNode content = new TestContentNode();
-		node.setContent(content);
 		Size contentSize = new Size(1, 1);
+		
+		TestNode child = new TestNode();
+		child.setSize(childSize);
+		
+		TestContentNode content = new TestContentNode();
 		content.setSize(contentSize);
+		
+		TestNode node = new TestNode();
 		node.addChild(child);
+		node.setContent(content);
+		node.setContentVisible(true);
+		
 		Layout layout = new Layout(node);
 		layout.doLayout();
 
@@ -62,8 +72,6 @@ public class LayoutTest {
 		int contentWidth = content.getSize().getWidth();
 		int childX = node.getChild(0).getPosition().getX();
 		assertThat(childX - contentX - contentWidth, is (Layout.GAP));
-		
-		
 		
 		assertThat(node.getContent().getPosition(), is (new Position(Layout.SPACE_AROUND, Layout.SPACE_AROUND)));
 		assertThat(node.getSize(), is(childSize));
