ebce5d224c7634a3a3afcfa2111502019cac6c14
davidvoelkel <der.voelkel.david@gmail.com>
2013-02-13 20:52:52 +0100

    letzer stand

---
+++ src/layoutproject/Layoutable.java
@@ -0,0 +1,12 @@
+package layoutproject;
+
+public interface Layoutable {
+
+	public Size getSize();
+
+	public void setSize(Size size);
+
+	public Position getPosition();
+
+	public void setPosition(Position position);
+}
