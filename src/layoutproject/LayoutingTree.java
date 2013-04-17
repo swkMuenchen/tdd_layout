d7161a442b724410860776cd7e24ab520ceaeceb
dpolivaev <dpolivaev>
2013-04-17 22:14:30 +0200

    double and triple sized root nodes

--- src/layoutproject/LayoutingTree.java
+++ src/layoutproject/LayoutingTree.java
@@ -25,15 +25,21 @@ public class LayoutingTree implements Tree {
 		else if (node.isHidden())
 			return getChildrenSize();
 		else {
-			return getNodeSize().extendHorizontally(
-				getChildrenSize().getWidth() + Node.X_GAP).extendVertically(
-				Node.Y_SHIFT);
+			Size size = getNodeSize().extendHorizontally(
+				getChildrenSize().getWidth() + Node.X_GAP);
+			size = size.extendVertically(Math.max(0, -child.getPosition()
+				.getY()));
+			return size;
 		}
 	}
 
 	public void layoutChildren() {
-		if (!isLeaf() && !node.isHidden())
-			child.setPosition(new Position(1 + Node.X_GAP, -Node.Y_SHIFT));
+		if (!isLeaf() && !node.isHidden()) {
+			int heightDiff = getNodeSize().getHeight()
+				- getChildrenSize().getHeight();
+			child.setPosition(new Position(1 + Node.X_GAP, heightDiff / 2
+				- Node.Y_SHIFT));
+		}
 	}
 
 	public boolean isLeaf() {
--- src/layoutproject/TreeTest.java
+++ src/layoutproject/TreeTest.java
@@ -51,10 +51,14 @@ public class TreeTest {
 		LayoutingTree parentTree = new LayoutingTree();
 		StubTree childTree = new StubTree(ONE_X_ONE);
 
-		parentTree.setRoot(new Node(ONE_X_ONE));
+		int nodeHeight = 1;
+		parentTree.setRoot(new Node(new Size(1, nodeHeight)));
 		parentTree.addChild(childTree);
 
-		Size parentTreeSize = new Size(2 + Node.X_GAP, 1 + Node.Y_SHIFT);
+		parentTree.layoutChildren();
+
+		int childY = (nodeHeight - 1) / 2 - Node.Y_SHIFT;
+		Size parentTreeSize = new Size(2 + Node.X_GAP, nodeHeight - childY);
 		assertThat(parentTree.getSize(), is(parentTreeSize));
 	}
 
@@ -70,6 +74,68 @@ public class TreeTest {
 
 		assertThat(childTree.getPosition(), //
 			is(new Position(1 + Node.X_GAP, -Node.Y_SHIFT)));
+	}
+
+	@Test
+	public void oneChildTreeWithDoubleSizedRootNode_setsChildPosition() {
+		LayoutingTree parentTree = new LayoutingTree();
+		StubTree childTree = new StubTree(ONE_X_ONE);
+
+		int nodeHeight = 2;
+		parentTree.setRoot(new Node(new Size(1, nodeHeight)));
+		parentTree.addChild(childTree);
+
+		parentTree.layoutChildren();
+
+		assertThat(childTree.getPosition(), //
+			is(new Position(1 + Node.X_GAP, -Node.Y_SHIFT)));
+	}
+
+	@Test
+	public void oneChildTreeWithDoubleSizedRootNode_setsTreeSize() {
+		LayoutingTree parentTree = new LayoutingTree();
+		StubTree childTree = new StubTree(ONE_X_ONE);
+
+		int nodeHeight = 2;
+		parentTree.setRoot(new Node(new Size(1, nodeHeight)));
+		parentTree.addChild(childTree);
+
+		parentTree.layoutChildren();
+
+		int childY = (nodeHeight - 1) / 2 - Node.Y_SHIFT;
+		Size parentTreeSize = new Size(2 + Node.X_GAP, nodeHeight - childY);
+		assertThat(parentTree.getSize(), is(parentTreeSize));
+	}
+
+	@Test
+	public void oneChildTreeWithTrippleSizedRootNode_setsChildPosition() {
+		LayoutingTree parentTree = new LayoutingTree();
+		StubTree childTree = new StubTree(ONE_X_ONE);
+
+		int nodeHeight = 3;
+		parentTree.setRoot(new Node(new Size(1, nodeHeight)));
+		parentTree.addChild(childTree);
+
+		parentTree.layoutChildren();
+
+		assertThat(childTree.getPosition(), //
+			is(new Position(1 + Node.X_GAP, 1 - Node.Y_SHIFT)));
+	}
+
+	@Test
+	public void oneChildTreeWithTrippleSizedRootNode_setsTreeSize() {
+		LayoutingTree parentTree = new LayoutingTree();
+		StubTree childTree = new StubTree(ONE_X_ONE);
+
+		int nodeHeight = 3;
+		parentTree.setRoot(new Node(new Size(1, nodeHeight)));
+		parentTree.addChild(childTree);
+
+		parentTree.layoutChildren();
+
+		int childY = 1 - Node.Y_SHIFT;
+		Size parentTreeSize = new Size(2 + Node.X_GAP, nodeHeight - childY);
+		assertThat(parentTree.getSize(), is(parentTreeSize));
 	}
 
 }
